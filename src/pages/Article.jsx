0727cfe5f68f9d26816e3b01bed723b03674c730
johnconnor <zkmd2012@inbox.ru>
2021-10-28 10:39:37 +0500

    7 useHistory redirect inside of useEffect function with 2 second timeout to let the user to read an error

--- src/pages/Article.jsx
+++ src/pages/Article.jsx
@@ -1,16 +1,22 @@
-import { useParams } from 'react-router-dom'
-import { useState } from 'react'
+import { useParams, useHistory } from 'react-router-dom'
+import { useEffect } from 'react'
 import { useFetch } from '../hooks/useFetch'
 
 const Article = () => {
-
-    //const [article, setArticle] = useState({})
 
     const {artId} = useParams()    
 
     const {data: article, isPending, error } = useFetch(`http://localhost:3000/articles/${artId}`)
 
-    
+    const history = useHistory()
+
+    useEffect(() => {
+        if (error) {
+            //redirect
+            setTimeout(() => history.push('/'), 3000) //to give the user time to read error message
+
+        }
+    }, [error, history]) //history was added to the dependancy array just because it is declared outside of useEffect
 
     return (
         <div>
