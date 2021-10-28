c9827b65ba7ccb8d026aaef7cba49a84cc67dea3
johnconnor <zkmd2012@inbox.ru>
2021-10-28 10:22:13 +0500

    6 useParams hook from react-router-dom. useFetch custom hook to fetch a single article

--- src/pages/Article.jsx
+++ src/pages/Article.jsx
@@ -1,7 +1,28 @@
+import { useParams } from 'react-router-dom'
+import { useState } from 'react'
+import { useFetch } from '../hooks/useFetch'
+
 const Article = () => {
+
+    //const [article, setArticle] = useState({})
+
+    const {artId} = useParams()    
+
+    const {data: article, isPending, error } = useFetch(`http://localhost:3000/articles/${artId}`)
+
+    
+
     return (
         <div>
-            <h1>Article</h1>
+            {isPending && <div>Loading...</div>}
+            {error && <div>{error}</div>}
+            {article && (
+                <div>
+                    <h2>{article.title}</h2>
+                    <p>By {article.author}</p>
+                    <p>{article.body}</p>
+                </div>
+            )}
         </div>
     )
 }
